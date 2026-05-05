aaed24fef1458bc40a102a302d882dbbbad003a5
Alain Tchiegang <atchiegang@gmail.com>
2026-05-05 16:49:36 +0100

    refactor(pricing): remove bootstrap config, switch to DB-driven fee policies

--- pricing-app/src/main/java/com/kratos/mok/pricing/app/PricingAppConfig.java
+++
@@ -1,9 +0,0 @@
-package com.kratos.mok.pricing.app;
-
-import com.kratos.mok.pricing.app.bootstrap.FeeBootstrapProperties;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@EnableConfigurationProperties(FeeBootstrapProperties.class)
-public class PricingAppConfig {}
