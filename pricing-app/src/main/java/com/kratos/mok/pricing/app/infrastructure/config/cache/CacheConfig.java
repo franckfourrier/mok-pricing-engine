eea3ea7070b6bc95b61f9d9627993ec81eba191b
Alain Tchiegang <atchiegang@gmail.com>
2026-06-06 13:02:25 +0100

    configuration en prod : keycloack : 90%

---
+++ pricing-app/src/main/java/com/kratos/mok/pricing/app/infrastructure/config/cache/CacheConfig.java
@@ -0,0 +1,9 @@
+package com.kratos.mok.pricing.app.infrastructure.config.cache;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableCaching
+public class CacheConfig {
+}
