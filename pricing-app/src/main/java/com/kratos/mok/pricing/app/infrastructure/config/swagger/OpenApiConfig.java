b3afb70b984bfc9f951d31f7b538e7907b76b27b
Alain Tchiegang <atchiegang@gmail.com>
2026-06-06 10:16:32 +0100

    configuration Swagger avec integration keyclock : ok

--- pricing-app/src/main/java/com/kratos/mok/pricing/app/infrastructure/config/swagger/OpenApiConfig.java
+++ pricing-app/src/main/java/com/kratos/mok/pricing/app/infrastructure/config/swagger/OpenApiConfig.java
@@ -3,8 +3,11 @@ package com.kratos.mok.pricing.app.infrastructure.config.swagger;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.OAuthFlow;
+import io.swagger.v3.oas.models.security.OAuthFlows;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,24 +16,52 @@ public class OpenApiConfig {
 
     public static final String BEARER_AUTH = "bearerAuth";
 
+    @Value("${mok.security.keycloak.server-url}")
+    private String serverUrl;
+
+    @Value("${mok.security.keycloak.realm}")
+    private String realm;
+
+    @Value("${mok.security.keycloak.client-id}")
+    private String clientId;
+
     @Bean
     public OpenAPI mokPricingOpenApi() {
+
+        String authUrl =
+                serverUrl +
+                        "/realms/" +
+                        realm +
+                        "/protocol/openid-connect/auth";
+
+        String tokenUrl =
+                serverUrl +
+                        "/realms/" +
+                        realm +
+                        "/protocol/openid-connect/token";
+
         return new OpenAPI()
                 .info(new Info()
                         .title("MoK Pricing Engine API")
                         .version("v1")
                         .description("Fees / Taxes / Commissions + Pricing breakdown"))
 
-                //schéma de sécurité
-                .components(new Components().addSecuritySchemes(
-                        BEARER_AUTH,
-                        new SecurityScheme()
-                                .type(SecurityScheme.Type.HTTP)
-                                .scheme("bearer")
-                                .bearerFormat("JWT")
-                ));
-
-                // applique l'auth par défaut à toute l'API
-                //.addSecurityItem(new SecurityRequirement().addList(BEARER_AUTH));
+                .components(new Components()
+                        .addSecuritySchemes(
+                                BEARER_AUTH,
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.OAUTH2)
+                                        .flows(
+                                                new OAuthFlows()
+                                                        .authorizationCode(
+                                                                new OAuthFlow()
+                                                                        .authorizationUrl(authUrl)
+                                                                        .tokenUrl(tokenUrl)
+                                                        )
+                                        )
+                        ))
+                .addSecurityItem(
+                        new SecurityRequirement().addList(BEARER_AUTH)
+                );
     }
 }
