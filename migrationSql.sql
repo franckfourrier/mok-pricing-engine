ac7e22524839782a27a3709e071cb3f624217391
Alain Tchiegang <atchiegang@gmail.com>
2026-06-11 14:47:58 +0100

    maj fichier migration

--- migrationSql.sql
+++ migrationSql.sql
@@ -13,9 +13,9 @@ CREATE INDEX idx_ledger_acc_time_xaf
     ON ledger_entries (account_code, occurred_at DESC)
     WHERE currency = 'XAF';
 
--- MAJ : ledger_dashboard_snapshot
+/*-- MAJ : ledger_dashboard_snapshot
 ALTER TABLE ledger_dashboard_snapshot
-    ADD COLUMN member_count BIGINT DEFAULT 0 NOT NULL;
+    ADD COLUMN member_count BIGINT DEFAULT 0 NOT NULL;*/
 
 -- Quand le système grossit : Partitioning recommandé
 CREATE TABLE ledger_entries_2026 PARTITION OF ledger_entries
